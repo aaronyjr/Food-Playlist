b8609061390dece3cf488bad550e50d59bd1b7b2
germainewong <gwwc07@gmail.com>
2023-12-12 22:31:55 +0800

    added search icon and checked icon when selected

--- app/src/main/java/com/example/pandacapstone/FoodPreferenceScreen.kt
+++ app/src/main/java/com/example/pandacapstone/FoodPreferenceScreen.kt
@@ -5,14 +5,20 @@ import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Divider
@@ -26,6 +32,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -95,14 +102,23 @@ fun ColumnItem(
                 // When the item is clicked, toggle isSelected and call onClick
                 isSelected = !isSelected
                 inputCuisinePreference = foodItem
-                Log.i("foodpanda", inputCuisinePreference)
                 onClick()
             }
-            // Set the background color based on whether the item is selected
-            .background(if (isSelected) Color.LightGray else Color.Transparent)
     ) {
-        Text(text = foodItem, modifier = Modifier.padding(top = 16.dp, bottom = 16.dp), fontSize = 14.sp)
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+            Text(text = foodItem, modifier = Modifier.padding(top = 16.dp, bottom = 16.dp), fontSize = 14.sp)
+            Spacer(modifier = Modifier.width(4.dp))
+            if (isSelected) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = "Selected",
+                    modifier = Modifier.size(24.dp),
+                    tint = colorResource(id = R.color.party_pink)
+                )
+            }
+        }
         Divider(color = colorResource(id = R.color.cool_grey))
+
     }
 }
 
@@ -135,7 +151,13 @@ fun SearchViewCuisine(
         singleLine = true,
         textStyle = TextStyle(
             color = Color.Black, fontSize = 18.sp
-        )
+        ),
+        leadingIcon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = "Search"
+            )
+        }
     )
 }
 
