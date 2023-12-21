6e74d6815fdcaefed309fee0526b3374c61fc610
germainewong <gwwc07@gmail.com>
2023-12-21 17:14:24 +0800

    removed quantity and set meal options -gw

--- app/src/main/java/com/example/pandacapstone/CustomisationScreen.kt
+++ app/src/main/java/com/example/pandacapstone/CustomisationScreen.kt
@@ -1,8 +1,6 @@
 package com.example.pandacapstone
 
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,46 +10,35 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarOutline
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlin.math.*
+import kotlin.math.roundToInt
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -74,30 +61,30 @@ fun CustomisationScreen(onNextButtonClicked: (Int, String, Int, Int, Int) -> Uni
     ) {
         Row() {
             Column(modifier = Modifier.padding(end = 16.dp)) {
-                Row(
-                    modifier = Modifier.height(52.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.txt_quantity),
-                        style = TextStyle(fontSize = 18.sp),
-                        modifier = Modifier.padding(end = 10.dp)
-                    )
-                }
+//                Row(
+//                    modifier = Modifier.height(52.dp),
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Text(
+//                        text = stringResource(id = R.string.txt_quantity),
+//                        style = TextStyle(fontSize = 18.sp),
+//                        modifier = Modifier.padding(end = 10.dp)
+//                    )
+//                }
 
-                Spacer(modifier = Modifier.height(10.dp))
-                Row(
-                    modifier = Modifier.height(52.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.txt_meal),
-                        style = TextStyle(fontSize = 18.sp),
-                        modifier = Modifier.padding(end = 10.dp)
-                    )
-                }
-
-                Spacer(modifier = Modifier.height(10.dp))
+//                Spacer(modifier = Modifier.height(10.dp))
+//                Row(
+//                    modifier = Modifier.height(52.dp),
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Text(
+//                        text = stringResource(id = R.string.txt_meal),
+//                        style = TextStyle(fontSize = 18.sp),
+//                        modifier = Modifier.padding(end = 10.dp)
+//                    )
+//                }
+//
+//                Spacer(modifier = Modifier.height(10.dp))
 
                 Row(
                     modifier = Modifier.height(52.dp),
@@ -112,76 +99,76 @@ fun CustomisationScreen(onNextButtonClicked: (Int, String, Int, Int, Int) -> Uni
             }
 
             Column {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    IconButton(
-                        onClick = { quantity.value -= 1 },
-                        colors = IconButtonDefaults.iconButtonColors(
-                            contentColor = colorResource(id = R.color.party_pink),
-                            disabledContainerColor = Color.Transparent,
-                            disabledContentColor = Color.LightGray
-                        ),
-                        enabled = quantity.value >= 2,
-                      modifier = Modifier
-                            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(10.dp))
-                            .size(width = 40.dp, height = 40.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Remove,
-                            contentDescription = "Minus",
-                            modifier = Modifier.size(width = 20.dp, height = 20.dp)
-                        )
-                    }
-
-                    Text(text = quantity.value.toString(), modifier = Modifier.padding(16.dp))
-
-                    IconButton(
-                        onClick = { quantity.value += 1 },
-                        colors = IconButtonDefaults.iconButtonColors(
-                            contentColor = colorResource(id = R.color.party_pink)
-                        ),
-                        modifier = Modifier
-                            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(10.dp))
-                            .size(width = 40.dp, height = 40.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = "Add",
-                            modifier = Modifier.size(width = 20.dp, height = 20.dp)
-                        )
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                Row(
-                    modifier = Modifier.height(52.dp),
-                ) {
-                    mealOptions.forEach { label ->
-                        Row(
-                            modifier = Modifier
-                                .height(56.dp)
-                                .selectable(
-                                    selected = (meal.value == label),
-                                    onClick = { meal.value = label },
-                                    role = Role.RadioButton
-                                )
-                                .padding(end = 16.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            RadioButton(
-                                modifier = Modifier.padding(end = 6.dp),
-                                selected = (meal.value == label),
-                                onClick = null, // null recommended for accessibility with screen readers
-                                colors = RadioButtonDefaults.colors(
-                                    selectedColor = colorResource(id = R.color.party_pink),
-                                )
-                            )
-                            Text(text = label)
-                        }
-                    }
-                }
-
-                Spacer(modifier = Modifier.height(10.dp))
+//                Row(verticalAlignment = Alignment.CenterVertically) {
+//                    IconButton(
+//                        onClick = { quantity.value -= 1 },
+//                        colors = IconButtonDefaults.iconButtonColors(
+//                            contentColor = colorResource(id = R.color.party_pink),
+//                            disabledContainerColor = Color.Transparent,
+//                            disabledContentColor = Color.LightGray
+//                        ),
+//                        enabled = quantity.value >= 2,
+//                      modifier = Modifier
+//                            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(10.dp))
+//                            .size(width = 40.dp, height = 40.dp)
+//                    ) {
+//                        Icon(
+//                            imageVector = Icons.Default.Remove,
+//                            contentDescription = "Minus",
+//                            modifier = Modifier.size(width = 20.dp, height = 20.dp)
+//                        )
+//                    }
+//
+//                    Text(text = quantity.value.toString(), modifier = Modifier.padding(16.dp))
+//
+//                    IconButton(
+//                        onClick = { quantity.value += 1 },
+//                        colors = IconButtonDefaults.iconButtonColors(
+//                            contentColor = colorResource(id = R.color.party_pink)
+//                        ),
+//                        modifier = Modifier
+//                            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(10.dp))
+//                            .size(width = 40.dp, height = 40.dp)
+//                    ) {
+//                        Icon(
+//                            imageVector = Icons.Default.Add,
+//                            contentDescription = "Add",
+//                            modifier = Modifier.size(width = 20.dp, height = 20.dp)
+//                        )
+//                    }
+//                }
+//
+//                Spacer(modifier = Modifier.height(10.dp))
+//
+//                Row(
+//                    modifier = Modifier.height(52.dp),
+//                ) {
+//                    mealOptions.forEach { label ->
+//                        Row(
+//                            modifier = Modifier
+//                                .height(56.dp)
+//                                .selectable(
+//                                    selected = (meal.value == label),
+//                                    onClick = { meal.value = label },
+//                                    role = Role.RadioButton
+//                                )
+//                                .padding(end = 16.dp),
+//                            verticalAlignment = Alignment.CenterVertically
+//                        ) {
+//                            RadioButton(
+//                                modifier = Modifier.padding(end = 6.dp),
+//                                selected = (meal.value == label),
+//                                onClick = null, // null recommended for accessibility with screen readers
+//                                colors = RadioButtonDefaults.colors(
+//                                    selectedColor = colorResource(id = R.color.party_pink),
+//                                )
+//                            )
+//                            Text(text = label)
+//                        }
+//                    }
+//                }
+//
+//                Spacer(modifier = Modifier.height(10.dp))
 
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
